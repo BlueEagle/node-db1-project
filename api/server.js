90316fe2d75ba5482433d10284cbd914db93e9d7
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-10 14:35:41 -0600

    who needs a heart when a heart can be broken?

--- api/server.js
+++ api/server.js
@@ -6,4 +6,22 @@ const server = express();
 
 server.use(express.json());
 
+// GET, POST, PUT, DELETE
+
+server.get("/accounts", (req, res) => {
+  db.select("*")
+    .from("Accounts")
+    .then((accounts) => res.status(200).json({ accounts }).end())
+    .catch((error) => res.status(500).json({ error }));
+});
+
+server.post("/accounts", (req, res) => {
+  const account = req.body;
+  db("accounts")
+    .insert(account)
+    .returning("id")
+    .then((idn) => res.status(201).json({ inserted: idn }).end())
+    .catch((error) => res.status(500).json({ error }));
+});
+
 module.exports = server;
